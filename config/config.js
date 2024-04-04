6fff7041bd40616b11095437784f6b8464fc4f1f
PK Singh <118387796+pksingh-sme@users.noreply.github.com>
2024-04-04 10:40:46 -0700

    Update config.js
    minor fix

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
         host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || 'mysql123',
-        database: process.env.DB_NAME || 'snapxxl',
+        user: process.env.DB_USER || 'rwuser',
+        password: process.env.DB_PASSWORD || 'mypass',
+        database: process.env.DB_NAME || 'mydb',
     },
     jwtSecret: process.env.JWT_SECRET || 'mysnapjwtsecret',
 };
